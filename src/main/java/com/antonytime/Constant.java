bc088c6e9ac1b4e0d0d40566b7197b4fd745cc2e
AntonyTime <Y.AntNik@Gmail.com>
2014-12-02 19:51:21 +0300

    add constants for mysql

--- src/main/java/com/antonytime/Constant.java
+++ src/main/java/com/antonytime/Constant.java
@@ -2,16 +2,35 @@ package com.antonytime;
 
 public class Constant {
 
-    private static final String consumerKey="pXX3zrDBM5rnSKTiV4PzGLAGp";
+    private static final String consumerKey = "pXX3zrDBM5rnSKTiV4PzGLAGp";
+    private static final String consumerSecret = "2gvb3CQdQHWESHVa91ov5Anba8VYs39ZgmRain1st0cuyrADME";
 
-    public static String getConsumerKey() {
+    private static final String URL = "jdbc:mysql://localhost:3306/followers";
+    private static final String USERNAME = "root";
+    private static final String USERPAWWSORD = "root";
+    private static final String QUERYGETFOLLOWERS = "REPLACE INTO followers VALUES(?,?)";
+
+    static String getConsumerKey() {
         return consumerKey;
     }
 
-    public static String getConsumerSecret() {
+    static String getConsumerSecret() {
         return consumerSecret;
     }
 
-    private static final String consumerSecret="2gvb3CQdQHWESHVa91ov5Anba8VYs39ZgmRain1st0cuyrADME";
+    public static String getUrl() {
+        return URL;
+    }
 
+    public static String getUsername() {
+        return USERNAME;
+    }
+
+    public static String getUserpawwsord() {
+        return USERPAWWSORD;
+    }
+
+    public static String getQuerygetfollowers() {
+        return QUERYGETFOLLOWERS;
+    }
 }
