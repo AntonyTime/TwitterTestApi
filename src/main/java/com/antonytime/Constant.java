6a3b4efb30efe31b524b74f49ca7cdf9a82bf990
AntonyTime <Y.AntNik@Gmail.com>
2014-12-07 15:56:22 +0300

    remove all myasql fields and getters

--- src/main/java/com/antonytime/Constant.java
+++ src/main/java/com/antonytime/Constant.java
@@ -5,39 +5,12 @@ public class Constant {
     private static final String consumerKey = "pXX3zrDBM5rnSKTiV4PzGLAGp";
     private static final String consumerSecret = "2gvb3CQdQHWESHVa91ov5Anba8VYs39ZgmRain1st0cuyrADME";
 
-    private static final String URL = "jdbc:mysql://localhost:3306/followers";
-    private static final String USERNAME = "root";
-    private static final String USERPAWWSORD = "root";
-
-    private static final String REPLACE_INTO_FOLLOWERS_VALUES = "REPLACE INTO followers VALUES(?)";
-    private static final String REPLACE_INTO_UNFOLLOWERS_VALUES = "REPLACE INTO unfollowers VALUES(?)";
-
     static String getConsumerKey() {
         return consumerKey;
     }
 
     static String getConsumerSecret() {
         return consumerSecret;
-    }
-
-    public static String getUrl() {
-        return URL;
-    }
-
-    public static String getUsername() {
-        return USERNAME;
-    }
-
-    public static String getUserpawwsord() {
-        return USERPAWWSORD;
-    }
-
-    public static String getReplaceIntoFollowersValues() {
-        return REPLACE_INTO_FOLLOWERS_VALUES;
-    }
-
-    public static String getReplaceIntoUnfollowersValues() {
-        return REPLACE_INTO_UNFOLLOWERS_VALUES;
     }
 
 }
