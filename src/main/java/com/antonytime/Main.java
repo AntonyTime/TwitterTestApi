4661b2cc438b5a38221fb3d65fdb54382f70367c
AntonyTime <Y.AntNik@Gmail.com>
2014-12-08 18:27:00 +0300

    refactor code

--- src/main/java/com/antonytime/Main.java
+++ src/main/java/com/antonytime/Main.java
@@ -1,18 +1,28 @@
 package com.antonytime;
 
+import twitter4j.IDs;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         TestApi testApi = new TestApi();
 
-        testApi.logIn();
+        testApi.login();
 
-        testApi.getFollowersDataBase();
+        List<Long> followersListFromDataBase = testApi.getFollowersFromDB();
+        if (followersListFromDataBase.isEmpty())
+            System.out.println("Can't determine unfollowers!");
 
-        testApi.getFollowersCollection();
+        List<Long> followersListFromServer = testApi.getFollowersFromServer();
+        if (followersListFromServer.isEmpty())
+            System.out.println("Can't determine unfollowers!");
 
-        testApi.getResult();
+        testApi.findUnfollowers(followersListFromDataBase, followersListFromServer);
+
+        testApi.saveFollowersToDB(followersListFromServer);
+        testApi.saveUnfollowersToDB(followersListFromDataBase);
     }
 
 }
